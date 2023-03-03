8edb80249236bb1d25a108e7891d6ca9a5abd737
comar <saywooong@gmail.com>
2023-03-04 00:07:21 +0900

    add: teddybearA

--- test.sh
+++ test.sh
@@ -3,4 +3,9 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate DSNeRF
 
-python run_nerf.py --config configs/co3d.txt --render_only
+if [[ -z $1 ]]; then
+  echo -e "\033[31mError: specify config as 'configs/bear.txt' \033[0m" >&2
+  exit 1
+fi
+
+python run_nerf.py --config $1 --render_only
